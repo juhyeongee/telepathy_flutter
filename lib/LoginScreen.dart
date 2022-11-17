0055797ec332f747bcfbcf9f0d9b9019c8b86d7c
juhyeong kim <90133367+juhyeongee@users.noreply.github.com>
2022-11-17 22:41:10 +0900

    feat: push notification, noti 발생시키는 함수 및 버튼 추가

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:telepathy_flutter/kakao_login.dart';
 import 'package:telepathy_flutter/main_view_model.dart';
+import 'functions/fcmController/fcmController.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -56,6 +57,21 @@ class _LoginScreenState extends State<LoginScreen> {
                   },
                   child: Text(
                     "카카오톡 로그아웃하기",
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white),
+                  onPressed: () {
+                    fcmController.sendPushNotification(
+                        userToken:
+                            "cfZ8XEdQq0AfkOSRjb7qRm:APA91bGCd0M7wmY-4W3YbAtt_iLizXy_qmmVuQy3abQ7Wmz96eiyydgFCrDxGssawOJpKnhAZlnqJjFRWou5BIuEY7Mwj_D-epHMQLdjIvC1IOFJ--4N4gb4G_7e5F9SGuODDK-BbUJG",
+                        title: "테스트 메세지",
+                        body: "임시 메세지 입니다");
+                  },
+                  child: Text(
+                    "push notification 보내기",
                   ),
                 )
                 //
