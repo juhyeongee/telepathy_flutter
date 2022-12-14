38271890f2ce6ef8c40a9400605face4bc4bd627
juhyeong kim <90133367+juhyeongee@users.noreply.github.com>
2022-12-14 20:50:36 +0900

    feat: 파이어베이스로 메세지 get(), 이후 화면에 body메세지 paint

--- lib/screens/Home/MailBoxScreen.dart
+++ lib/screens/Home/MailBoxScreen.dart
@@ -1,7 +1,41 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:go_router/go_router.dart';
+
+final firestore = FirebaseFirestore.instance;
+
+Future<Object?> getMyReceivedMessage() async {
+  DocumentSnapshot messageBodyResult = await firestore
+      .collection('messageData')
+      .doc("01053618962")
+      .collection("receivedMessage")
+      .doc("01030088962")
+      .get();
+
+  Map<String, dynamic> converted =
+      jsonDecode(jsonEncode(messageBodyResult.data()));
+  String messageBody = converted['body'];
+
+  return messageBody;
+}
+
+Future<Object?> getMySentMessage() async {
+  DocumentSnapshot result = await firestore
+      .collection('messageData')
+      .doc("01053618962")
+      .collection("sentMessage")
+      .doc("01030088962")
+      .get();
+
+  Map<String, dynamic> converted = jsonDecode(jsonEncode(result.data()));
+  String messageBody = converted['body'];
+
+  return messageBody;
+}
 
 class MailBoxScreen extends StatefulWidget {
   const MailBoxScreen({super.key});
@@ -11,24 +45,45 @@ class MailBoxScreen extends StatefulWidget {
 }
 
 class _MailBoxScreenState extends State<MailBoxScreen> {
+  var myMessage = [];
+
+  void initState() {
+    super.initState();
+  }
+
+  getMyMessage() async {
+    var result =
+        await firestore.collection('messageData').doc("01053618962").get();
+    return result;
+  }
+
+  sendMyNewMessage() {
+    firestore
+        .collection("messageData")
+        .doc("01053618962")
+        .set({"to": "01011111111", "from": "010", "message": "코드로 보내본 메세지"});
+  }
+
+  routeToWriteMessagingScreen() {
+    context.go("/homeScreen/writeMessage");
+  }
+
   @override
   Widget build(BuildContext context) {
-    void initState() {
-      super.initState();
-    }
-
-    routeToWriteMessagingScreen() {
-      context.go("/homeScreen/writeMessage");
-    }
-
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(children: <Widget>[
-          MessageContainer(),
-          MessageContainer(),
-          MessageContainer(),
-
+          Text(
+            "보낸 메세지",
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          SentMessageBoxes(),
+          Text(
+            "받은 메세지",
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          ReceivedMessageBoxes(),
           //  텍스트 필드. 텍스트필드에 controller를 등록하여 리스너를 통한 핸들링
           ElevatedButton(
               onPressed: routeToWriteMessagingScreen,
@@ -39,8 +94,84 @@ class _MailBoxScreenState extends State<MailBoxScreen> {
   }
 }
 
+class SentMessageBoxes extends StatefulWidget {
+  const SentMessageBoxes({super.key});
+
+  @override
+  State<SentMessageBoxes> createState() => _SentMessageBoxesState();
+}
+
+class _SentMessageBoxesState extends State<SentMessageBoxes> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getMySentMessage(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData == false) {
+          return CircularProgressIndicator();
+        }
+        //error가 발생하게 될 경우 반환하게 되는 부분
+        else if (snapshot.hasError) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Error: ${snapshot.error}',
+              style: TextStyle(fontSize: 15),
+            ),
+          );
+        }
+        // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 것이다.
+        else {
+          return MessageContainer(
+            result: snapshot.data,
+          );
+        }
+      },
+    );
+  }
+}
+
+class ReceivedMessageBoxes extends StatefulWidget {
+  const ReceivedMessageBoxes({super.key});
+
+  @override
+  State<ReceivedMessageBoxes> createState() => _ReceivedMessageBoxesState();
+}
+
+class _ReceivedMessageBoxesState extends State<ReceivedMessageBoxes> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getMyReceivedMessage(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData == false) {
+          return CircularProgressIndicator();
+        }
+        //error가 발생하게 될 경우 반환하게 되는 부분
+        else if (snapshot.hasError) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Error: ${snapshot.error}',
+              style: TextStyle(fontSize: 15),
+            ),
+          );
+        }
+        // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 것이다.
+        else {
+          return MessageContainer(
+            result: snapshot.data,
+          );
+        }
+      },
+    );
+  }
+}
+
 class MessageContainer extends StatelessWidget {
-  const MessageContainer({super.key});
+  const MessageContainer({super.key, required this.result, this.phoneNumber});
+  final result;
+  final phoneNumber;
 
   @override
   Widget build(BuildContext context) {
@@ -63,14 +194,14 @@ class MessageContainer extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Text(
-                  "미지의 친구",
+                  "010-3008-8962",
                   style: TextStyle(
                       fontFamily: "neodgm",
                       color: Color(0xff72D4A5),
                       fontSize: 14),
                 ),
                 Text(
-                  "텔레파시에 가입한 것을 환영합니다",
+                  "$result",
                   style: TextStyle(
                       fontFamily: "neodgm",
                       color: Color(0xff72D4A5),
